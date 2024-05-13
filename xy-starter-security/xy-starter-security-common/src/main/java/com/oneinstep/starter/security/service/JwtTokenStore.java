1de515ba65f50d21e8be22017ef973842b0ed68c
aaron <oneinstep@gmail.com>
2024-05-13 16:46:36 +0800

    add cache

--- xy-starter-security/xy-starter-security-common/src/main/java/com/oneinstep/starter/security/service/JwtTokenStore.java
+++ xy-starter-security/xy-starter-security-common/src/main/java/com/oneinstep/starter/security/service/JwtTokenStore.java
@@ -1,11 +1,15 @@
 package com.oneinstep.starter.security.service;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.oneinstep.starter.security.constant.SystemType;
 import jakarta.annotation.Resource;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.redisson.api.RBucket;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
@@ -23,22 +27,42 @@ public class JwtTokenStore {
     @Resource
     private RedissonClient redissonClient;
 
+    private final Cache<String, String> jwtCache = Caffeine.newBuilder()
+            .expireAfterWrite(600, TimeUnit.SECONDS)
+            .maximumSize(1000)
+            .build();
+
     private static final String USER_JWT_PREFIX = "xy:jwt:%s:%d";
 
     public void storeToken(@NotNull SystemType systemType, @NonNull Long userId, @NotEmpty String jwt) {
-        RBucket<String> bucket = redissonClient.getBucket(String.format(USER_JWT_PREFIX, systemType.name(), userId));
+        String key = String.format(USER_JWT_PREFIX, systemType.name(), userId);
+
+        RBucket<String> bucket = redissonClient.getBucket(key);
         bucket.set(jwt);
         bucket.expire(Duration.of(1, TimeUnit.DAYS.toChronoUnit()));
+
+        jwtCache.put(key, jwt);
     }
 
     public void removeToken(@NotNull SystemType systemType, @NonNull Long userId) {
-        RBucket<String> bucket = redissonClient.getBucket(String.format(USER_JWT_PREFIX, systemType.name(), userId));
+        String key = String.format(USER_JWT_PREFIX, systemType.name(), userId);
+        RBucket<String> bucket = redissonClient.getBucket(key);
         bucket.delete();
+
+        jwtCache.invalidate(key);
     }
 
     public boolean isTokenExistAndRight(@NotNull SystemType systemType, @NonNull Long userId, @NotEmpty String jwt) {
-        RBucket<String> bucket = redissonClient.getBucket(String.format(USER_JWT_PREFIX, systemType.name(), userId));
-        return bucket.isExists() && jwt.equals(bucket.get());
+        String key = String.format(USER_JWT_PREFIX, systemType.name(), userId);
+        String jwtFromCache = jwtCache.get(key, this::getTokenFromRedis);
+        log.info("jwt from cache: {}", jwtFromCache);
+        return StringUtils.isNotBlank(jwtFromCache) && jwtFromCache.equals(jwt);
+    }
+
+    private String getTokenFromRedis(String key) {
+        log.info("get token from redis, key: {}", key);
+        RBucket<String> bucket = redissonClient.getBucket(key);
+        return bucket.get();
     }
 
 }
